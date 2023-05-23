1a3509b1e8f42bd16e74bac7fe3bf78ca374bae8
Guillaume <guillaumecolloch56@gmail.com>
2023-05-23 13:11:14 +0200

    ajout des boules de cristales

--- Front-end/src/components/Banner.jsx
+++ Front-end/src/components/Banner.jsx
@@ -3,7 +3,11 @@ import '../styles/components/_banner.scss'
 const Banner = () => {
   return (
     <div className="background-image-block">
-      <div className="background-image"></div>
+      <div className="background-image">
+      <img className="overlay-image-left" src="./media/header/dbz.png" alt="Left Image" />
+      <img className="overlay-image-right" src="./media/header/dbz.png" alt="right Image" />
+      <img className="background-overlay" src="./media/header/figurines.png" alt="Overlay Image" />
+      </div>
       <img className="overlay-image" src="./media/header/forme1.png" alt="Overlay Image" />
     </div>
   );
