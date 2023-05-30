1b6878db7248d6a5ac02811a6b7fdd5bc1ed47f0
Guillaume <guillaumecolloch56@gmail.com>
2023-05-30 17:55:40 +0200

    ajout responsive boules de cristales banner

--- Front-end/src/components/Présentation.jsx
+++ Front-end/src/components/Présentation.jsx
@@ -8,66 +8,45 @@ const Présentation = () => {
   <input type="radio" name="slider" id="s2"/>
   <input type="radio" name="slider" id="s5"/>
   <label for="s1" id="slide1">
-  <div className="product">
-                <div className="image2">
+  <div className="productCaroussel">
+                <div className="image2Caroussel namePriceCaroussel">
+                    <h3>Manga One piece</h3>
                     <img src="/media/mangas/Naruto.jpg" />
                 </div>
-                <div className="namePrice">
-                    <h3>Manga One piece</h3>
+                <div className="namePriceCaroussel">
                     <span>6,99 €</span>
                 </div>
                 <p>Manga de l'animé/manga One Piece</p>
-                <div className="stars">
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                </div>
-                <div className="bay">
+                <div className="bayCaroussel">
                     <button>voir le produit</button>
                 </div>
             </div>
         </label>
-  <label for="s2" id="slide2"><div className="product">
-                <div className="image2">
+  <label for="s2" id="slide2"><div className="productCaroussel">
+                <div className="image2Caroussel namePriceCaroussel" >
+                    <h3>Manga One piece</h3>
                     <img src="/media/mangas/Naruto.jpg" />
                 </div>
-                <div className="namePrice">
-                    <h3>Manga One piece</h3>
+                <div className="namePriceCaroussel">
                     <span>6,99 €</span>
                 </div>
                 <p>Manga de l'animé/manga One Piece</p>
-                <div className="stars">
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                </div>
-                <div className="bay">
+                <div className="bayCaroussel">
                     <button>voir le produit</button>
                 </div>
             </div>
             </label>
   <label for="s5" id="slide3">
-  <div className="product">
-                <div className="image2">
+  <div className="productCaroussel">
+                <div className="image2Caroussel namePriceCaroussel">
+                    <h3>Manga One piece</h3>
                     <img src="/media/mangas/Naruto.jpg" />
                 </div>
-                <div className="namePrice">
-                    <h3>Manga One piece</h3>
+                <div className="namePriceCaroussel">
                     <span>6,99 €</span>
                 </div>
                 <p>Manga de l'animé/manga One Piece</p>
-                <div className="stars">
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                </div>
-                <div className="bay">
+                <div className="bayCaroussel">
                     <button>voir le produit</button>
                 </div>
             </div>
